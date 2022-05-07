dc98d01b31b01e793762938627f90c4bdbca8df2
robert <robert@robert-ThinkPad-T450s>
2022-05-07 14:46:06 -0400

    added radius and changed it on the Dodecahedron
    practice

--- NamedSolids.groovy
+++ NamedSolids.groovy
@@ -1,6 +1,7 @@
 double size =20
+double radius =40
 return [
-new Dodecahedron(size).toCSG().movex(size*0),
+new Dodecahedron(radius).toCSG().movex(size*0),
 new Icosahedron(size).toCSG().movex(size*2),
 new Octahedron(size).toCSG().movex(size*4),
 new Tetrahedron(size).toCSG().movex(size*6),
